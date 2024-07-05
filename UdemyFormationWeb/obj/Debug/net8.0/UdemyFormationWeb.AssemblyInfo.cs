dcd1f8d0a51d360ca86c9b8ec51a792ecd5ea810
Paul Chaumont <pchaumont@ensc.fr>
2024-07-05 20:34:13 +0200

    add production app setinggs

--- UdemyFormationWeb/obj/Debug/net8.0/UdemyFormationWeb.AssemblyInfo.cs
+++ UdemyFormationWeb/obj/Debug/net8.0/UdemyFormationWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UdemyFormationWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afb50cad49ce48861b36843e05d40973a4b70e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183fff5ae45b7577669063667d50911e12aa9971")]
 [assembly: System.Reflection.AssemblyProductAttribute("UdemyFormationWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UdemyFormationWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
